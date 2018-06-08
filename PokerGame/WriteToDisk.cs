57b8f2b0cd9f6ca112da6448a29969f88a7c39fe
Marko Stuurman <marko.rayman@gmail.com>
2018-06-08 08:46:27 +0200

    Poker project

---
+++ PokerGame/WriteToDisk.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public enum WinState
+{
+    PLAYER1,
+    PLAYER2,
+    TIE
+}
+
+public class WriteToDisk
+{
+    public static string[] output = new string[1000];
+
+    public static void WriteRoundToDisk()
+    {
+        System.IO.File.WriteAllLines(@"E:\output.txt", output);
+    }
+    
+    public static void AddOutput(int round, WinState state)
+    {
+        output[round] = "round " + (round+1) + " " + state;
+    }
+}
